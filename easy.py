39511c514ffe7a3cc8801d80f982b314a5417edd
Darwin Osingo <osingoosteen@gmail.com>
2026-07-08 12:10:12 +0300

    basics building up

--- easy.py
+++ easy.py
@@ -1,13 +1,13 @@
-#I.	Write a Python statement that reads an exam score from the user, converts it to an integer, and stores it in a variable called score.
-#II.	Write an if-elif-else block that assigns a letter grade based on the score:  90–100 = 'A'  |  80–89 = 'B'  |  70–79 = 'C'  |  60–69 = 'D'  |  below 60 = 'F' Also print: 'Pass' if grade is A, B, or C, otherwise print 'Fail'.
-#III.	Explain what would happen if the elif score >= 60 condition was placed BEFORE the elif score >= 90 condition in your chain. Why does ordering matter in an if-elif-else structure?
-#IV.	Explain the concept of inheritance in object-oriented programming. Give a real-world analogy and a Python code example showing a parent class Animal and a subclass Dog that inherits from it. The Dog subclass must override at least one method.
-#V.	Explain polymorphism >in object-oriented programming. Write a code example showing a function process_shape(shape) that works correctly with both a Circle and a Rectangle through polymorphism, without using isinstance() or type() checks inside the function.
-
-        
+def dot_product(v1,v2):
+    if len(v1) != len(v2):
+        raise   ValueError("The lenth of the values dont match!!")
+    result = 0
+    for i in range( len(v1)):
+        result += v1[i]*v2[i]
+    return result
     
-score = int(input("What did you get in your last exam?:"))
-if score > 100 or score < 0:
-    print("Please enter a valid score ")
-    else score => 90:
-        
+
+
+    
+
+assert dot_product([1, 2, 3], [4, 5, 6]) == 32
