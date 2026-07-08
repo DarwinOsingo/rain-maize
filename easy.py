58090131dfb92188ca3c57139cc11aa9968676a5
Darwin Osingo <osingoosteen@gmail.com>
2026-07-08 15:16:32 +0300

    toward attention we go

--- easy.py
+++ easy.py
@@ -1,3 +1,4 @@
+import math
 def dot_product(v1, v2):
     if len(v1) != len(v2):
         raise ValueError("The length of the vectors don't match")
@@ -5,6 +6,18 @@ def dot_product(v1, v2):
     for i in range(len(v1)):
         result += v1[i] * v2[i]
     return result
+
+
+def magnitude(v):
+    return math.sqrt(dot_product(v,v))
+def angle_between(v1,v2):
+    angle = dot_product(v1,v2)/(magnitude(v1)*magnitude (v2))
+    cos = math.acos(angle)
+    return cos
+    
+    
+
+
 
 
 if __name__ == "__main__":
