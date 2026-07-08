a1d7008a0595371a57f555c16097661544a64db3
Darwin Osingo <osingoosteen@gmail.com>
2026-07-08 12:25:11 +0300

    basics building up

--- easy.py
+++ easy.py
@@ -1,13 +1,13 @@
-def dot_product(v1,v2):
+def dot_product(v1, v2):
     if len(v1) != len(v2):
-        raise   ValueError("The lenth of the values dont match!!")
+        raise ValueError("The length of the vectors don't match")
     result = 0
-    for i in range( len(v1)):
-        result += v1[i]*v2[i]
+    for i in range(len(v1)):
+        result += v1[i] * v2[i]
     return result
-    
 
 
-    
-
-assert dot_product([1, 2, 3], [4, 5, 6]) == 32
+if __name__ == "__main__":
+    res = dot_product([1, 2, 3], [4, 5, 6])
+    print(res)
+    assert res == 32
