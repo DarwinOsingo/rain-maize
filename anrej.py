6e81dc8bf7bb1d98271a03e00087e7ec326fe067
Darwin Osingo <osingoosteen@gmail.com>
2026-07-18 11:54:16 +0300

    well

--- anrej.py
+++ anrej.py
@@ -11,11 +11,24 @@ encode = lambda s:[stoi[c] for c in s]
 decode = lambda d : ''.join([itos[i] for i in d])
 print(encode("Hii there"))
 print(decode(encode("Hii there")))
+
 data = torch.tensor(encode(text),dtype=torch.long)
 print(data.shape )
 print(data[:100])
+
 n = int(0.9*len(data))
 train = data[:n]
 val = data[n:]
+
 block_size = 8
+batch_size = 4
 train[:block_size+1]
+x = train[:block_size]
+y = train[1:block_size+1]
+
+torch.manual_seed(42)
+def get_batches(split):
+    data = train if split == "train" else val
+    ix = torch.randint[len(data)-block_size,(4,)]
+
+
--- try.py
+++ try.py
@@ -1,5 +1,9 @@
-chars = ['a', 'b', 'c', 'd']
-stoi = {}
-for i,ch in enumerate(chars):
-    stoi[ch]=i
-print (stoi)
+import torch
+data =torch.tensor([i for i in range(1,1001)],dtype=torch.long)
+context = 12
+
+ix = torch.randint(len(data)-context,(4,))
+x = torch.stack([data[i:context+i] for i in ix])
+y = torch.stack([data[i+1:context+1+i] for i in ix])
+print(x)
+print(y)
