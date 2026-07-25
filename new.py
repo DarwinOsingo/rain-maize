6dd1d30c75dfbfa43bb959236389ad528064ae58
Darwin Osingo <osingoosteen@gmail.com>
2026-07-25 20:08:51 +0300

    back

--- new.py
+++ new.py
@@ -5,7 +5,7 @@ class Book:
         self.is_borrowed = False    
 
 class Member :
-    def __init__(self,name,books_taken):
+    def __init__(self,name):
         self.name = name
         self.books_taken = []
 class Library:
@@ -27,24 +27,27 @@ class Library:
             print("The book you typed is not in our system please retry")
             return
         
-        elif book.is_burrowed:
+        elif book.is_borrowed:
             print("The book has been borrowed and will be returned at a later date")
         else:
-            book.is_taken = True
+            book.is_borrowed = True
             member.books_taken.append(book)
             
             print(f"You have succesfully borrowed {book.title}")
     def return_book(self,book,member):
+        
         if member not in self.members:
             print("You cannot return a book if you arent a member")
             return
-        elif book.is_borrowed:
-            print("Thankyou for returning the book")
-            self.book.append(book)
-            book.is_taken = False
+        if book in member.books_taken:
+        
+            print("Thank you for returning the book")
+            
+            book.is_borrowed = False
+            member.books_taken.remove(book)
         else:
             print("You cannot return a book that you havent taken out buddy")
-    def show_books(self,books):
+    def show_books(self):
         print("="*50)
         print("\n WELCOME TO THE LIBRARY")
         print("="*50)
@@ -53,10 +56,11 @@ class Library:
             print(f"\n{book.title} status :{status}")
     def show_member_books(self,member):
         print("="*50)
-        print(f"\n WELCOME TO THE LIBRARY :{member} These are your books!")
+        print(f"\n WELCOME TO THE LIBRARY :{member.name} These are your books!")
         print("="*50)
         for index,book in enumerate(member.books_taken,start=1):
-            print(f"\n  {index}:{book}")
+            print(f"\n  {index}:{book.title}")
+
 
 
 
