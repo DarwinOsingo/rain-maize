2b230c86ae84c93ac087d0328c87244c3b6d4a9f
Darwin Osingo <osingoosteen@gmail.com>
2026-08-13 11:55:54 +0300

    we proceed

--- new.py
+++ new.py
@@ -1,6 +1,15 @@
-products = ["Laptop", "Phone", "Tablet", "Monitor"]
-prices = [80000, 50000, 30000, 25000]
+products = [
+    {"name": "Laptop", "price": 80000},
+    {"name": "Phone", "price": 50000},
+    {"name": "Tablet", "price": 30000},
+    {"name": "Monitor", "price": 25000}
+]
+highest = max(products,key =lambda price:price["price"])
+lowest = min(products,key=lambda price:price["price"])
 
-elecs = dict(zip(products,prices))
-for products,prices in elecs.items():
-    print(f"{products:<10}:{prices}")
+print(f"The most expesnive device is {highest["name"]} at {highest["price"]}")
+
+print(f"The least expensive is {lowest["name"]} at {lowest["price"]}")
+
+
+
