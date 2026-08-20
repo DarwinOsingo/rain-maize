e90dc067f3462a8e7c28e1ea2cd208a44f2ca614
Darwin Osingo <osingoosteen@gmail.com>
2026-08-20 14:00:54 +0300

    where we stand

--- new.py
+++ new.py
@@ -1,10 +1,3 @@
-students = [
-    {"name": "Alice", "score": 82},
-    {"name": "Bob", "score": 48},
-    {"name": "Charlie", "score": 91},
-    {"name": "David", "score": 76}
-]
-above_90 = any(student["score"] >90 for student in students)
-above_50 = all(student["score"] >50 for student in students)
-print(above_50)
-print(above_90)
+num = [1,2,3,4,4,4,4,4,5,56,6,4,6]
+total = sum(num)
+print(total)
--- try.py
+++ try.py
@@ -1,14 +1,22 @@
-import torch 
-with open("shadow-slave/shadow-slave/0001 - Chapter 1 Nightmare Begins.txt","r") as f:
-    text = f.read()
-char = sorted(list(set(text)))
-stoi = {ch:i for i,ch in enumerate(char)}
-itos = {i:ch for i,ch in enumerate(char)}
-encode = lambda s:[stoi[c] for c in s ]
-decode = lambda d : "".join(itos[c] for c in d)
-print(encode("Helo"))
-print(decode([19, 39, 46, 49]))
-def stoi():
-    stoi ={}
-    for i,ch in enumerate(char):
-        stoi[ch] = i
+employees = [
+    {"name": "Alice", "department": "Engineering", "salary": 80000},
+    {"name": "Bob", "department": "Sales", "salary": 60000},
+    {"name": "Charlie", "department": "Engineering", "salary": 90000},
+    {"name": "David", "department": "HR", "salary": 55000},
+    {"name": "Eve", "department": "Sales", "salary": 70000},
+    {"name": "Frank", "department": "Engineering", "salary": 75000}
+]
+highest = []
+
+for employee in employees:
+    salary = employee["salary"]
+    name = employee["name"]
+    if salary > 70000:
+        highest.append(name)
+print(highest)
+
+
+
+
+
+       
