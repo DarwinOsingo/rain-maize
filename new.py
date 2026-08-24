e82dda735f0f1f72f087ae16abfd809a391a1da5
Darwin Osingo <osingoosteen@gmail.com>
2026-08-24 21:58:31 +0300

    3sum

--- new.py
+++ new.py
@@ -1,2 +1,4 @@
-numbers = [2, 7, 11, 15]
-print(range(len(numbers)))
+nums = [3, -2, 1, 0, -1, 2, -3, 4]
+target = 2
+nums.sort()
+print(nums)
--- try.py
+++ try.py
@@ -1,8 +1,18 @@
-nums = [-4, -1, -1, 0, 1, 2]
-target = 1
+nums = [3, -2, 1, 0, -1, 2, -3, 4]
+target = 2
+nums.sort()
+pairs =[]
+for i in range(len(nums)):
+    left = i+1
+    right = len(nums)-1
+    while left < right:
+        total = nums[i]+nums[left]+nums[right]
+        if total == target:
+            pairs.append([nums[right],nums[left],nums[i]])
+        elif total > target:
+            right -=1
+        else:
+            left+=1
+print(pairs)
 
-left = 2
-right = 5
-while left < right:
-    sum = nums[left]+nums[right]
-    
+
