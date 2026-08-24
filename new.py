2ef9aec105f9f89086b61f502d3ddb0ebc38f867
Darwin Osingo <osingoosteen@gmail.com>
2026-08-24 08:39:17 +0300

    forgot

--- new.py
+++ new.py
@@ -1,3 +1,2 @@
-numbers = [1, 2, 3, 4]
-squares = [num*2 for num in numbers if num % 2 == 0]
-print(squares)
+numbers = [2, 7, 11, 15]
+print(range(len(numbers)))
--- try.py
+++ try.py
@@ -1,7 +1,8 @@
-numbers = [2, 7, 11, 15]
-target = 9
+nums = [-4, -1, -1, 0, 1, 2]
+target = 1
 
-for i in range(len(numbers)):
-    for j in range(len(numbers)):
-        if numbers[i] + numbers[j] == target:
-            print([i, j])
+left = 2
+right = 5
+while left < right:
+    sum = nums[left]+nums[right]
+    
