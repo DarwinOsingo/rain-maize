1a9fb853b1c269a8333ec7aec6eb1449e64cb793
Darwin Osingo <osingoosteen@gmail.com>
2026-08-27 20:37:25 +0300

    beyond bigram

--- titan.py
+++ titan.py
@@ -1,60 +1,34 @@
 text = """
-the cat sat on the mat
-the cat ate the fish
-the dog sat on the rug
-the dog ate the bone
-the dog chased the cat
-the cat ran up the tree
-a cat sat on the wall
-a dog barked at the cat
-the cat slept on the mat
-the dog slept on the rug
-a bird sang in the tree
-the bird flew over the wall
-the cat watched the bird
-the dog watched the cat
-a boy walked the dog
-the boy fed the cat
-the boy fed the dog
-the cat drank the milk
-the dog drank the water
-a girl played with the cat
-the girl played with the dog
+he quick brown fox jumps over the lazy dog. Pack my box with five dozen liquor jugs!
+"Where," she asked, "did you put the keys?" He replied: "I left them on the shelf."
+Jinxed wizards pluck ivy from the big quilt; zebras march quickly, dodging trees.
+It's a fine day (mostly) -- warm, bright, and calm. Don't you think so? I do.
+Mr. Bloxby's cat, aged 12, ran 3 laps around the yard -- a new record!
+"That's amazing," said Val; "we've never seen it move that fast."
+Prices ranged from $5 to $99.99, depending on size & color.
 """
-vocab = text.split()
-lib = {}
-for i in range(len(vocab)-1):
-    current_word = vocab[i]
-    next_word = vocab[i+1]
-    if current_word not in lib:
-        lib[current_word]={}
-    if next_word not in lib[current_word]:
-        lib[current_word][next_word]=1
-    else:
-        lib[current_word][next_word]+=1
-prob={}
-for x in lib:
-    prob[x]={}
-    total = sum(lib[x].values())
-    for next_word,freq in lib[x].items():
-        final = freq/total
-        prob[x][next_word]=final
-def generate(start_word,n):
-    sentense = []
-    if start_word not in prob:
-        return(f'The word {start_word} isnt in our DB try again')
-    sentense.append(start_word)
-    while len(sentense)<n:
-        after= prob[start_word]
-        high = max(after,key=after.get)
-        sentense.append(high)
-        start_word=high
-        
-    return " ".join(sentense)
+vocab = list(text)
+words = sorted(list(set(vocab)))
+words_to_ints = {ch:i for i,ch in enumerate(words)}
+# for i,ch in enumerate( words):
+#     if i not in words_to_ints:
+#         words_to_ints[i]=ch
+ints_to_words = {i:ch for i,ch in enumerate(words)}
+# for i,ch in enumerate(words):
+#     if i not in ints_to_words:
+#         ints_to_words[i]=ch
 
-print(generate("the",7))
-    
+encode = lambda s:[ words_to_ints[l] for l in s ]
+decode = lambda t:"".join([ints_to_words[k]for k in t])
+print(encode("boy"))
+print(decode([16, 2, 17, 2, 12, 23, 18, 10, 18]))
+#now I need to make x and y such that x +1 = y so that they correspond simple enough I could use a for loop 
+#but im not sure what exactly to loop over 
+data = encode(text)
+x = data[:-1]
+y=data[1:]
+print(len(words))
 
-        
+
 
 
